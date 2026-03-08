b403c4275a724ea47362cb0022b51f5f2490af20
sadam-agashirinov <sadam.agashirinov@gmail.com>
2026-03-08 06:47:40 +0300

    feat: fix gitignore

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf23123a3c7c2fcf78eb1518b446f9b68fdea12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82db6ef056c3141b1766daafc96a9cd967ec511f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
